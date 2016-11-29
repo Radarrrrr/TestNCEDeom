a740809dd739cc25003d04371b72365105528fb5
Radarrrrr <imryd@163.com>
2016-11-29 18:16:23 +0800

    add notification service

---
+++ NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by Radar on 2016/11/29.
+//  Copyright © 2016年 Radar. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ NotificationService/NotificationService.m
@@ -0,0 +1,80 @@
+//
+//  NotificationService.m
+//  NotificationService
+//
+//  Created by Radar on 2016/11/29.
+//  Copyright © 2016年 Radar. All rights reserved.
+//
+
+#import "NotificationService.h"
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.contentHandler = contentHandler;
+    self.bestAttemptContent = [request.content mutableCopy];
+    
+    // Modify the notification content here...
+    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
+    
+    
+    
+    // 重写一些东西
+    self.bestAttemptContent.title = @"我是标题";
+    self.bestAttemptContent.subtitle = @"我是子标题";
+    self.bestAttemptContent.body = @"来自徐不同";
+    
+    // 附件
+//    NSDictionary *dict =  self.bestAttemptContent.userInfo;
+//    NSDictionary *notiDict = dict[@"aps"];
+//    NSString *imgUrl = [NSString stringWithFormat:@"%@",notiDict[@"imageAbsoluteString"]];
+    
+    // 这里添加一些点击事件，可以在收到通知的时候，添加，也可以在拦截通知的这个扩展中添加
+    
+//    self.bestAttemptContent.categoryIdentifier = @"category1";
+    
+    
+    
+    
+//    if (!imgUrl.length) {
+//        
+//        self.contentHandler(self.bestAttemptContent);
+//        
+//    }
+    
+    
+    
+//    [self loadAttachmentForUrlString:imgUrl withType:@"png" completionHandle:^(UNNotificationAttachment *attach) {
+//        
+//        if (attach) {
+//            self.bestAttemptContent.attachments = [NSArray arrayWithObject:attach];
+//        }
+//        self.contentHandler(self.bestAttemptContent);
+//        
+//    }];
+    
+    
+
+    
+    
+    
+    
+    
+    self.contentHandler(self.bestAttemptContent);
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
