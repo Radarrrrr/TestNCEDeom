48476c5bfa06532b4cdeea359ec84db913a89823
Radarrrrr <imryd@163.com>
2016-12-01 11:17:41 +0800

    add some download

--- NotificationService/NotificationService.m
+++ NotificationService/NotificationService.m
@@ -32,42 +32,33 @@
     self.bestAttemptContent.body = @"Y的终于调通了";
     
     // 附件
-//    NSDictionary *dict =  self.bestAttemptContent.userInfo;
+    NSDictionary *dict =  self.bestAttemptContent.userInfo;
 //    NSDictionary *notiDict = dict[@"aps"];
 //    NSString *imgUrl = [NSString stringWithFormat:@"%@",notiDict[@"imageAbsoluteString"]];
     
-    // 这里添加一些点击事件，可以在收到通知的时候，添加，也可以在拦截通知的这个扩展中添加
+    NSString *imgUrl = dict[@"image"];
+    if(!imgUrl || [imgUrl isEqualToString:@""]) 
+    {
+        self.contentHandler(self.bestAttemptContent);
+    }
     
+    
+    // 这里添加一些点击事件，可以在收到通知的时候，添加，也可以在拦截通知的这个扩展中添加
 //    self.bestAttemptContent.categoryIdentifier = @"category1";
     
     
     
+    //放在最后
+    [self loadAttachmentForUrlString:imgUrl withType:@"jpg" completionHandle:^(UNNotificationAttachment *attach) {
+        
+        if (attach) {
+            self.bestAttemptContent.attachments = [NSArray arrayWithObject:attach];
+        }
+        self.contentHandler(self.bestAttemptContent);
+        
+    }];
     
-//    if (!imgUrl.length) {
-//        
-//        self.contentHandler(self.bestAttemptContent);
-//        
-//    }
-    
-    
-    
-//    [self loadAttachmentForUrlString:imgUrl withType:@"png" completionHandle:^(UNNotificationAttachment *attach) {
-//        
-//        if (attach) {
-//            self.bestAttemptContent.attachments = [NSArray arrayWithObject:attach];
-//        }
-//        self.contentHandler(self.bestAttemptContent);
-//        
-//    }];
-    
-    
-
-    
-    
-    
-    
-    
-    self.contentHandler(self.bestAttemptContent);
+//    self.contentHandler(self.bestAttemptContent);
 }
 
 - (void)serviceExtensionTimeWillExpire {
@@ -75,5 +66,64 @@
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
     self.contentHandler(self.bestAttemptContent);
 }
+
+
+
+- (void)loadAttachmentForUrlString:(NSString *)urlStr
+                          withType:(NSString *)type
+                  completionHandle:(void(^)(UNNotificationAttachment *attach))completionHandler{
+    
+    
+    __block UNNotificationAttachment *attachment = nil;
+    NSURL *attachmentURL = [NSURL URLWithString:urlStr];
+    NSString *fileExt = [self fileExtensionForMediaType:type];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    [[session downloadTaskWithURL:attachmentURL
+                completionHandler:^(NSURL *temporaryFileLocation, NSURLResponse *response, NSError *error) {
+                    if (error != nil) {
+                        NSLog(@"%@", error.localizedDescription);
+                    } else {
+                        NSFileManager *fileManager = [NSFileManager defaultManager];
+                        NSURL *localURL = [NSURL fileURLWithPath:[temporaryFileLocation.path stringByAppendingString:fileExt]];
+                        [fileManager moveItemAtURL:temporaryFileLocation toURL:localURL error:&error];
+                        
+                        NSError *attachmentError = nil;
+                        attachment = [UNNotificationAttachment attachmentWithIdentifier:@"" URL:localURL options:nil error:&attachmentError];
+                        if (attachmentError) {
+                            NSLog(@"%@", attachmentError.localizedDescription);
+                        }
+                    }
+                    
+                    completionHandler(attachment);
+                    
+                    
+                }] resume];
+    
+    
+    
+}
+- (NSString *)fileExtensionForMediaType:(NSString *)type {
+    NSString *ext = type;
+    
+    
+    if ([type isEqualToString:@"image"]) {
+        ext = @"jpg";
+    }
+    
+    if ([type isEqualToString:@"video"]) {
+        ext = @"mp4";
+    }
+    
+    if ([type isEqualToString:@"audio"]) {
+        ext = @"mp3";
+    }
+    
+    return [@"." stringByAppendingString:ext];
+}
+
+
+
+
 
 @end
